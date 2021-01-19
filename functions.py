0713dfe58c41c58e0c51da0f0cb16ead8094071b
noahwest <nolwest@ucsc.edu>
2021-01-19 01:36:21 -0800

    template commit
    Introduces main.py/functions.py structure.
    main.py runs the program, functions.py has the functions.
    Opens webcam in 720p, and closes the program when q is pressed.
    Some small functions carried from old main are in functions.py

--- functions.py
+++ functions.py
@@ -1,2 +1,24 @@
-def hello_world():
-    print("Hello, world!\n")
+import cv2
+
+# Takes the result from GUI user decision on input source.
+# Defaults to 0 for default webcam as input.
+def get_videocapture_arg():
+    return 0
+
+def set_cap_height_and_width(cap, height, width):
+    #3 == width, 4 == height
+    HEIGHT_CONSTANT = 3
+    WIDTH_CONSTANT = 4
+    cap.set(HEIGHT_CONSTANT, height)
+    cap.set(WIDTH_CONSTANT, width)
+
+def resize_frame(frame, width, height):
+    cv2.resize(frame, (width, height))
+
+def display_boxes(boxes, frame):
+    for (xA, yA, xB, yB) in boxes:
+        point_one = (xA, yA)
+        point_two = (xB, yB)
+        color = (0, 255, 0)
+        line_width = 2
+        cv2.rectangle(frame, point_one, point_two, color, line_width)
--- main.py
+++ main.py
@@ -3,5 +3,20 @@ import cv2
 import math
 import functions as fn
 
-if __name__ == '__main__':
-    fn.hello_world()
+if __name__ == "__main__":
+
+    cv2.startWindowThread()
+    cap = cv2.VideoCapture(fn.get_videocapture_arg())
+    fn.set_cap_height_and_width(cap, 1280, 720)
+
+    while(True):
+
+        ret, frame = cap.read()
+
+        cv2.imshow('press q to exit', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
