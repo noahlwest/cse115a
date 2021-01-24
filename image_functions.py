d7eb65ff999a512fcde33cf0dab952b8247d778b
noahwest <nolwest@ucsc.edu>
2021-01-24 00:36:33 -0800

    yolov3 first commit
    Runs slowly, should learn how to use CUDA/cuDNN to speed it up

--- image_functions.py
+++ image_functions.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import distance_functions
 
 
@@ -10,8 +11,7 @@ def init_opencv():
     cv2.startWindowThread()
 
 
-def stop_opencv(cap):
-    cap.release()
+def stop_opencv():
     cv2.destroyAllWindows()
     cv2.waitKey(1)
 
@@ -76,25 +76,32 @@ def get_person_base_pixel_location():
 
 def start_human_detection_loop(height, angle, fov):
     print("[+] Human detection started")
+    model, classes, colors, output_layers = load_yolo()
+    cap = start_videocapture("webcam", "none")
 
     while(True):
         ret, frame = cap.read()
-        if ret:
-            #boxes_around_people = detect_people(frame)
-            #display_boxes(boxes_around_people, frame)
+        height, width, channels = frame.shape
+        blob, outputs = detect_objects(frame, model, output_layers)
+        boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
+        draw_labels(boxes, confs, colors, class_ids, classes, frame)
+        key = cv2.waitKey(1)
+        if key == 27:
+        	break
+	
+    cap.release()
 
-            #vert_positions = get_people_base_pixel_location(boxes_around_people)
-            #distances, lines = distance_functions.find_distances_between_positions(vert_positions)
-            #(function this)
-            #for index, distance in enumerate(distances):
-            #   if distance < 6ft
-            #       display_line_and_distance(lines[index], distance)
+        #boxes_around_people = detect_people(frame)
+        #display_boxes(boxes_around_people, frame)
 
-            #show the output, wait for 'esc' press
-            cv2.imshow('press esc to exit', frame)
-            key = cv2.waitKey(1)
-            if key == 27:
-                break
+        #vert_positions = get_people_base_pixel_location(boxes_around_people)
+        #distances, lines = distance_functions.find_distances_between_positions(vert_positions)
+        #(function this)
+        #for index, distance in enumerate(distances):
+        #   if distance < 6ft
+        #       display_line_and_distance(lines[index], distance)
+
+        #show the output, wait for 'esc' press
 
     # some loop to yoink and analize frames
     #print("[+] Simulated human found")
@@ -106,3 +113,67 @@ def start_human_detection_loop(height, angle, fov):
 
     #print("[+] Continue simulation...")
     print("[+] Ending detection...")
+
+def load_yolo():
+	net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+	classes = []
+	with open("coco.names", "r") as f:
+		classes = [line.strip() for line in f.readlines()]
+
+	layers_names = net.getLayerNames()
+	output_layers = [layers_names[i[0]-1] for i in net.getUnconnectedOutLayers()]
+	colors = np.random.uniform(0, 255, size=(len(classes), 3))
+	return net, classes, colors, output_layers
+
+def display_blob(blob):
+	'''
+		Three images each for RED, GREEN, BLUE channel
+	'''
+	for b in blob:
+		for n, imgb in enumerate(b):
+			cv2.imshow(str(n), imgb)
+
+def detect_objects(img, net, outputLayers):			
+	blob = cv2.dnn.blobFromImage(img, scalefactor=0.00392, size=(320, 320), mean=(0, 0, 0), swapRB=True, crop=False)
+	net.setInput(blob)
+	outputs = net.forward(outputLayers)
+	return blob, outputs
+
+def get_box_dimensions(outputs, height, width):
+	boxes = []
+	confs = []
+	class_ids = []
+	for output in outputs:
+		for detect in output:
+			scores = detect[5:]
+			class_id = np.argmax(scores)
+			conf = scores[class_id]
+			if conf > 0.3:
+				center_x = int(detect[0] * width)
+				center_y = int(detect[1] * height)
+				w = int(detect[2] * width)
+				h = int(detect[3] * height)
+				x = int(center_x - w/2)
+				y = int(center_y - h / 2)
+				boxes.append([x, y, w, h])
+				confs.append(float(conf))
+				class_ids.append(class_id)
+	return boxes, confs, class_ids
+
+def draw_labels(boxes, confs, colors, class_ids, classes, img): 
+    indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
+    font = cv2.FONT_HERSHEY_PLAIN
+    x, y, w, h = boxes[0]
+    label = str(classes[class_ids[0]])
+    color = colors[0]
+    cv2.rectangle(img, (x, y), (x+w, y+h), color, 2)
+    cv2.putText(img, label, (x, y - 5), font, 1, color, 1)
+
+	#for i in range(len(boxes)):
+	#	if i in indexes:
+	#		x, y, w, h = boxes[i]
+	#		label = str(classes[class_ids[i]])
+	#		color = colors[i]
+	#		cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+	#		cv2.putText(img, label, (x, y - 5), font, 1, color, 1)
+    cv2.imshow("Press 'esc' to exit", img)
--- main.py
+++ main.py
@@ -5,23 +5,16 @@ import image_functions as img
 import calibration as cal
 import distance_functions as dist
 import gui_functions as gui
-import mapping
 
 if __name__ == "__main__":
 
-    img.hello_world()
-    cal.hello_world()
-    dist.hello_world()
-    gui.hello_world()
-    mapping.hello_world()
     print("Starting program...")
     
     webcam = True
     img.init_opencv()
-    cap = img.start_videocapture("webcam", "none")
 
     height, angle, fov = gui.display_gui_calibration()
     
     img.start_human_detection_loop(height, angle, fov)
 
-    img.stop_opencv(cap)
+    img.stop_opencv()
