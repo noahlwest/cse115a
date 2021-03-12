a1fdd84b6cbb859580b2c8728d275ed695a8c702
Timothy Chandler <50037485+tdkchandler@users.noreply.github.com>
2021-03-12 13:32:16 -0800

    Update distance_functions.py

--- distance_functions.py
+++ distance_functions.py
@@ -1,5 +1,6 @@
 import math
 
+
 # Input:       angle, fov, vertical position
 # Output:      final angle
 # Description: adjust the angle based on the vertical position, fov, and initial angle
@@ -9,6 +10,7 @@ def adjust_angle(angle, fov, vertical_position):
     # any angle > 90 is an error or false detection
     return final_angle
 
+
 # Input:       fov, horz ****NEED AN EXPLANATION ON HORZ****
 # Output:      final angle
 # Description: adjusts the horizontal angle based on the given inputs
@@ -16,6 +18,7 @@ def adjust_angle_horz(fov, horz):
     final_angle = ((horz / 1280) * fov)
     # any angle > 90 is an error or false detection
     return final_angle
+
 
 # Input:       angle, fov, vertical position
 # Output:      final angle
@@ -26,19 +29,16 @@ def adjust_angle_vert(angle, fov, vertical_position):
     # any angle > 90 is an error or false detection
     return final_angle
 
+
 # Input:       camera height, camera angle, camera fov, vertical postion of a person's feet
 # Output:      distance
 # Description: calculates the distance between the camera and the person's feet
 def find_distance(height, angle, fov, vertical_position_of_persons_feet):
-    #print("height:", height)
-    #print("angle:", angle)
-    #print("fov:", fov)
-    #print("vertical_position_of_persons_feet:", 100 - vertical_position_of_persons_feet * 100)
     final_angle = adjust_angle(angle, fov, 100 - vertical_position_of_persons_feet * 100)
-    #print("final_angle:", final_angle)
     final_angle = math.radians(final_angle)
     distance = height * (1 / math.cos(final_angle))
     return distance
+
 
 # Input:       radial distance of point a, polar angle of point a, azimuth (angular measurement) of point a
 #              radial distance of point b, polar angle of point b, azimuth (angular measurement) of point b
@@ -51,11 +51,12 @@ def distance_between_points(a_radial_dis, a_polar_ang, a_azimuthal_ang, b_radial
     b_azimuthal_ang = math.radians(b_azimuthal_ang)
     b_polar_ang = math.radians(b_polar_ang)
 
-    sin_shit = (math.sin(a_polar_ang) * math.sin(b_polar_ang) * math.cos(a_azimuthal_ang - b_azimuthal_ang) +
+    sin_eq = (math.sin(a_polar_ang) * math.sin(b_polar_ang) * math.cos(a_azimuthal_ang - b_azimuthal_ang) +
                 math.cos(a_polar_ang) * math.cos(b_polar_ang))
-    final = math.sqrt(a_radial_dis ** 2 + b_radial_dis ** 2 - (2 * a_radial_dis * b_radial_dis * sin_shit))
+    final = math.sqrt(a_radial_dis ** 2 + b_radial_dis ** 2 - (2 * a_radial_dis * b_radial_dis * sin_eq))
 
     return final
+
 
 # Input:       coords of person 1, coords of person 2, vertical fov, horizontal fov, camera angle,
 #              distance to person 1, distance to person 2
@@ -68,7 +69,5 @@ def return_distance(xy_tuple1, xy_tuple2, v_fov, h_fov, angle, dist1, dist2):
     x2 = adjust_angle_horz(h_fov, x2)
     y1 = adjust_angle_vert(angle, v_fov, y1)
     y2 = adjust_angle_vert(angle, v_fov, y2)
-
-    #print(dist1, y1, x1, dist2, y2, x2)
 
     return distance_between_points(dist1, y1, x1, dist2, y2, x2)
